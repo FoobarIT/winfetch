5bc6605a4901cb654802185bca1feb9d7b2fc083
Dany D <55385221+FoobarIT@users.noreply.github.com>
2022-04-17 10:03:54 +0200

    Winfetch first version

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # WINFETCH 
 ### Simple neofetch clone for windows.
 
+#### For a come back with Python, I decided to clone a quite famous application which is Neofetch. For the moment the script works only on OPS Windows. 
+
+# PREVIEW
+![alt-text](https://i.ibb.co/WnKhGQk/Screenshot-3.png)
+
+# TODO
+- Multiple platform support.
+- Why not add device analysis features. (IDK)
+- Refactor code.
--- winfetch.py
+++ winfetch.py
@@ -1,8 +1,10 @@
+#! 
 import os
-import sys
 import platform
 import ctypes
- 
+import psutil
+
+
 def windows_uptime():
     lib = ctypes.windll.kernel32
     t = lib.GetTickCount64()
@@ -10,9 +12,10 @@ def windows_uptime():
     mins, sec = divmod(t, 60)
     hour, mins = divmod(mins, 60)
     days, hour = divmod(hour, 24)
-    return (f"{days} days, {hour:02}:{mins:02}:{sec:02}")
+    return f"{days} days, {hour:02}:{mins:02}:{sec:02}"
 
-class typo: 
+
+class typo:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
     DARKCYAN = '\033[36m'
@@ -23,30 +26,35 @@ class typo:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
+
+
 class system:
     USERNAME = os.environ.get('USERNAME')
     HOSTNAME = os.getenv('COMPUTERNAME')
     OS = platform.system()
-    OS_RELEASE = platform.release()
-    OS_UPTIME = windows_uptime()
+    KERNEL = platform.uname()[3]
+    RELEASE = platform.release()
+    UPTIME = windows_uptime()
+    PROC = platform.processor()
+    GPU = platform.uname()[4]
+    MEM_USAGE = psutil.virtual_memory()[2] # (total, available, percent, used, free, active, inactive, wired)
+    CPU_USAGE = psutil.cpu_percent(4)
 
-print(os.name()[2])
 
-print(typo.BLUE + "                 ....iilllllllllllll" + typo.END + "          "+typo.BOLD+ system.USERNAME +"@"+ system.HOSTNAME + typo.END+"")
-print(typo.BLUE + "     ....iillll  lllllllllllllllllll" + typo.END + "          =====================")
-print(typo.BLUE + " iillllllllllll  lllllllllllllllllll" + typo.END + "          "+typo.BOLD +"OS: "+system.OS+" "+system.OS_RELEASE+typo.END+"")
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END + "          "+typo.BOLD +"HOST: "+system.HOSTNAME+typo.END+"")
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END + "          "+typo.BOLD +"Uptime: "+system.OS_UPTIME+typo.END+"")
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + "" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " llllllllllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + " `^^^^^^lllllll  lllllllllllllllllll" + typo.END)
-print(typo.BLUE + "       ````^^^^  ^^lllllllllllllllll" + typo.END)
-print(typo.BLUE + "                      ````^^^^^^llll" + typo.END)
-
+print(typo.BLUE + "                  ....iilllllllllllll" + typo.END + "    " + typo.BOLD + system.USERNAME + "@" + system.HOSTNAME + typo.END + "")
+print(typo.BLUE + "     ....iillll   lllllllllllllllllll" + typo.END + "    " + typo.BLUE+ "========================" + "")
+print(typo.BLUE + " iillllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "OS: " + system.OS + " " + system.RELEASE + typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "HOST: " + system.HOSTNAME + typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "Kernel: " + system.KERNEL + typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "Proc: " + system.PROC + typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "GPU: " + system.GPU + typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "Uptime: " + system.UPTIME + typo.END + "")
+print("")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "Mem Usage: "+ str(system.MEM_USAGE) +"%"+ typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END + "    " + typo.BOLD + "CPU Usage: "+ str(system.CPU_USAGE) +"%"+ typo.END + "")
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END)
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END)
+print(typo.BLUE + " llllllllllllll   lllllllllllllllllll" + typo.END)
+print(typo.BLUE + " `^^^^^^lllllll   lllllllllllllllllll" + typo.END)
+print(typo.BLUE + "       ````^^^^   ^^lllllllllllllllll" + typo.END)
+print(typo.BLUE + "                       ````^^^^^^llll" + typo.END)
